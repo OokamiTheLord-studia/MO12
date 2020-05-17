219c721664ec68166e40c56217659ec2d51fe275
OokamiTheLord <kuba@axod.pl>
2020-05-17 21:49:29 +0200

    added interpolated function and getting evenly distributed nodes function

--- MO12.cpp
+++ MO12.cpp
@@ -1,16 +1,36 @@
 ﻿#include <iostream>
-#include <math.h>
+#include <cmath>
+#include <tuple>
+#include <functional>
+#include <array>
+#include <vector>
 
-double interpolated_function(double x)
+using namespace std;
+
+
+const std::function<double(double)> interpolated_function = [](double x) {return 1 / (1 + 10 * pow(x, 6)); };
+
+
+ std::vector<std::pair<double, double>>* get_evenly_distributed_nodes(double begin, double end, int node_quantity, std::function<double(double)> func)
 {
-    return 1 / (1 + 10 * pow(x, 6));
+    double interval = (end - begin) / node_quantity;
+    std::vector<std::pair<double, double>>* vec = new std::vector<std::pair<double, double>>();
+    vec->reserve(node_quantity);
+    for (double i = begin; i < end; i += interval)
+    {
+        vec->push_back(std::pair<double, double>(i, func(i)));
+    }
+    vec->push_back(std::pair<double, double>(end, func(end)));
+    return vec;
 }
-
-
 
 
 int main()
 {
-    
+    auto evenly_distributed_nodes = get_evenly_distributed_nodes(-1, 1, 100, interpolated_function);
+    for (auto i = evenly_distributed_nodes->begin(); i != evenly_distributed_nodes->end(); i++)
+    {
+        cout << i->first << '\t' << i->second << endl;
+    }
 }
 
